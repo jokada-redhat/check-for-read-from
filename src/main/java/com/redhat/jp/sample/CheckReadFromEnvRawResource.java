a4ea5a7cba8c3b6f36ef52e7bd04134da4f7ce05
jokada-redhat <34223312+jokada-redhat@users.noreply.github.com>
2024-08-05 14:32:08 +0900

    add read from system.getproperty

--- src/main/java/com/redhat/jp/sample/CheckReadFromEnvRawResource.java
+++ src/main/java/com/redhat/jp/sample/CheckReadFromEnvRawResource.java
@@ -1,5 +1,6 @@
 package com.redhat.jp.sample;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
@@ -9,7 +10,9 @@ public class CheckReadFromEnvRawResource {
 
     @GET
     public Response readFromEnvironment() {
-        return Response.ok(System.getProperty("DUMMY_ENV_RAW")).build();
+        String value = System.getProperty("DUMMY_ENV_RAW");
+        Log.infov("Read env: {0}={1}", "DUMMY_ENV_RAW", value);
+        return Response.ok(value).build();
     }
 
 }
--- src/main/java/com/redhat/jp/sample/CheckReadFromEnvResource.java
+++ src/main/java/com/redhat/jp/sample/CheckReadFromEnvResource.java
@@ -2,6 +2,7 @@ package com.redhat.jp.sample;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.quarkus.logging.Log;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
@@ -14,6 +15,7 @@ public class CheckReadFromEnvResource {
 
     @GET
     public Response readFromEnvironment() {
+        Log.infov("Read env: {0}={1}", "DUMMY_ENV", value1);
         return Response.ok(value1).build();
     }
 
